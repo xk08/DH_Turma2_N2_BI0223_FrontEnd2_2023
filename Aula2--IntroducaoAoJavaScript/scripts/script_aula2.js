8343eb13ab05e1bf2ed7ba8ddaee1a5d29b3b7d8
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-02-13 22:36:23 -0300

    Aula2, arquivos base p/ aula

---
+++ Aula2--IntroducaoAoJavaScript/scripts/script_aula2.js
@@ -0,0 +1,31 @@
+/* @@@@ EXIBIÇÃO E CAPTURA DOS DADOS DO USUÁRIO
+- Alert()
+- Confirm()
+- Prompt()
+*/
+
+//Indo além do console.log...
+//@@ ALERT()
+
+
+//@@ CONFIRM()
+
+
+//@@ PROMPT()
+
+
+/// @@@ CONVERSÃO DE DADOS EM JS
+
+
+/// @@@ FUNÇÕES MATEMÁTICAS EM JS
+
+
+/* @@@@ Dicas Mesa de Trabalho: Por onde começar a realizar a atividade ??
+1 - Capturar a escolha de vocês (1, 2 ou 3)
+    - Sugiro converter para inteiro e guardar em uma variável.
+2 - Capturar a escolha da "máquina". 
+    - Fazer isso, usando o Math.Random() com algum intervalo;
+3 - Testar (Switch e IF) quem foi o ganhador/perdedor da rodada, ou se empatou.
+4 - Guardar em variáveis separadas, quem ganhou cada rodada
+5 - Ao final, verificar e declarar um campeão (melhor de 2 rodadas)
+*/

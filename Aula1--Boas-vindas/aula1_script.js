2942c09d45a5922facd0369bb2af22f675a502db
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-02-13 22:35:26 -0300

    Aula 1, dia 13/02/23, códigos feitos em aula

---
+++ Aula1--Boas-vindas/aula1_script.js
@@ -0,0 +1,14 @@
+//Declarando variável textual e exibindo no console do navegador
+let nome = "Ronaldo";
+console.log("Seja bem-vindo " + nome);
+
+//Declarando um array misto em JS
+let meuArray = ["Robson", "Ronaldo", "Gustavo", 34, false];
+
+//Adicionado uma informação no Array
+meuArray.push("Novo nome");
+
+//Percorrendo o array de elementos e exibindo os valores no console
+for (let i = 0; i < meuArray.length; i++) {
+    console.log(meuArray[i]);
+}

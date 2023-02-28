5948cc69760794a6c405ba83a594063691efda34
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-02-27 22:24:34 -0300

    Aula 5, arquivos base p/ aula

---
+++ Aula5--DOM_ModificarElementosComJs/Aula/scripts/script_aula5.js
@@ -0,0 +1,10 @@
+// [1] - Modelos de texto  - Template Strings
+
+
+// [2] - Modificando elementos com o DOM
+
+
+// [3] - Modificando as propriedades de estilização dos elementos
+
+
+// [4] Modificando a classe dos elementos

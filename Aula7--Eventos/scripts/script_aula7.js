3c9657759a8adcf481d4131927c52059e8253f29
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-03-09 12:13:31 -0300

    Aula 7 Eventos: Arquivos base p/ aula

---
+++ Aula7--Eventos/scripts/script_aula7.js
@@ -0,0 +1,24 @@
+/* Atributos dinâmicos */
+
+/*
+Principais tipos de eventos que temos:
+- Eventos de tela/pagina
+- Eventos de formulários
+- Eventos de mouse
+- Eventos de teclado
+*/
+
+///@@@@Eventos de janela/pagina
+
+
+///@@@@Eventos de formulários
+
+
+///@@@ Eventos do mouse (onClick)
+
+
+///@@@ Eventos do mouse, além do onClick
+
+
+///@@@@@ Eventos de teclado
+

5641d7caa8d5902773b9ea5c5e277a6f3f585696
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-03-09 22:28:04 -0300

    Aula 8, arquivos  base p/ aula

---
+++ Aula8--DisparandoEventos/scripts/script_aula8.js
@@ -0,0 +1,16 @@
+/* Tipos de funções em JS */
+
+// Função declarada ou declarativa
+
+
+// Funções expressadas ou funções de expressão
+
+
+// Funções aninhadas - Closures 
+
+
+// Funções de CallBacks (Função que recebe uma função como parametro e a executa)
+
+
+// Funções de Seta (=>) ou ArrowFunctions
+

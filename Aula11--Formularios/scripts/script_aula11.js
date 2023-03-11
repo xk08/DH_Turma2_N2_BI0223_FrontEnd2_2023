342c3faa92fa5873315704b9255d2c295760a54e
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-03-10 22:50:09 -0300

    Atualizando repo com as últimas aulas

---
+++ Aula11--Formularios/scripts/script_aula11.js
@@ -0,0 +1,3 @@
+/* 1 - Capturar as informações vindas do HTML e obter os valores com o JS */
+
+/* 2 - Normalizar as informações obtidas */
--- Aula8--DisparandoEventos/scripts/script_aula8.js
+++ Aula8--DisparandoEventos/scripts/script_aula8.js
@@ -2,15 +2,75 @@
 
 // Função declarada ou declarativa
 
+let usuario = {
+    nome: "Marcos",
+    idade: 25
+}
+
+let informacaoRetornada = boasVindasUsuario(usuario);
+console.log(informacaoRetornada);
+
+function boasVindasUsuario(usuarioRecebido) { //Assinatura da função
+    //Escopo desta função
+    //return console.log(`O usuario ${usuarioRecebido.nome} possui  ${usuarioRecebido.idade} anos`)
+    return `O usuario ${usuarioRecebido.nome} possui ${usuarioRecebido.idade} anos`;
+}
+
 
 // Funções expressadas ou funções de expressão
+let exibeIdade = function () {
+    console.log("A idade do usuário é 25 anos");
+}
+
+let exibeIdade = () => console.log("A idade do usuário é 25 anos");
+exibeIdade();
+
 
 
 // Funções aninhadas - Closures 
 
+function boasVindas() {
+
+    function exibeNomeUsuario() {
+        //console.log("O nome é: Marcos");
+    }
+
+    exibeNomeUsuario();
+
+}
+
+boasVindas();
+
 
 // Funções de CallBacks (Função que recebe uma função como parametro e a executa)
+let executaCalculo = function (funcaoRecebidaPorCallback) {
 
+    let numero1 = 10;
+    let numero2 = 20;
+    let resultado = funcaoRecebidaPorCallback(numero1, numero2); //Executando a função recebida por parametro
+    console.log(resultado);
 
-// Funções de Seta (=>) ou ArrowFunctions
+}
 
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+
+}
+function subtrair(numero1, numero2) {
+    return numero1 - numero2;
+
+}
+
+executaCalculo(somar);
+executaCalculo(subtrair);
+
+//Exemplos de função de callBack que já utilizamos
+
+let frutas = ["Maça", "Pera", "Uva"];
+frutas.forEach(function (fruta) {
+    console.log(fruta);
+})
+
+let btn = document.getElementById("botaoEnviar");
+btn.addEventListener("click", function () {
+});

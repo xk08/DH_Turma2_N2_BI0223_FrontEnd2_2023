55be816e04300045d35814d4e9e0085c36444b27
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-03-21 22:22:47 -0300

    Aula 16, códigos feitos em aula + aula 17, arquivos base

--- Aula16--IntroducaoAoAssincronismo/Aula/js/index.js
+++ Aula16--IntroducaoAoAssincronismo/Aula/js/index.js
@@ -1,2 +1,44 @@
 /* Aula 16 - Introdução ao assincronimso e Promises */
 /* Nesta aula estamos apenas "Simulando" requisições assincronas ao utilizar as promises */
+
+let validacao = true;
+
+let novaPromisse = new Promise(function (resolve, reject) {
+
+    setTimeout(() => {
+
+        if (validacao) { //Validação passou
+            resolve("Promisse deu certo");
+
+        } else {
+            reject("A promisse falhou");
+        }
+
+    }, 3000);
+})
+
+//Resolvendo a promisse
+novaPromisse.then(
+    function (resultado) {
+        console.log("Primeiro Then");
+        //console.log(resultado);
+        return resultado;
+    }
+).then(
+    function (resultado) {
+        console.log("Segundo Then");
+        //console.log(resultado);
+        exibeResultado(resultado);
+    }
+)
+    .catch(
+        function (erro) {
+            console.log(erro);
+        }
+    );
+
+
+function exibeResultado(resultado) {
+    console.log(resultado);
+}
+

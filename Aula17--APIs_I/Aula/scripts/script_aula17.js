4ae07348dd15026912ade8561c0222c51a7d38df
Marcos Vinnicius Martins <gronytzki@gmail.com>
2023-03-22 22:19:12 -0300

    Aula 17, dia 22/03/23, códigos feitos em aula

--- Aula17--APIs_I/Aula/scripts/script_aula17.js
+++ Aula17--APIs_I/Aula/scripts/script_aula17.js
@@ -6,9 +6,25 @@ botaoPesquisar.addEventListener('click', function (evento) {
     evento.preventDefault();
 
     if (quantidadeFormulario.value != "") {
-        //Executar a busca a API...
+        // BaseURL + EndPoint  = URL da API/Servidor
+        fetch(`https://dog.ceo/api/breeds/image/random/${quantidadeFormulario.value}`)
+            .then(
+                //Resolve a primeira parte da promise, mas, ainda não tem acesso aos dados do corpo da resposta.
+                function (resultado) {
+                    return resultado.json(); // transforma o objeto json (assincrono) para objeto js literal
+                }
+            )
+            .then(
+                function (data) {
+                    //Tem acesso ao conteúdo 'data' vindo da resposta da requisição
+                    renderizaFotosCachorrosDom(data); //envia o objeto recebido para uma função específica
+                })
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                }
+            )
 
-        
     } else {
         alert("Você deve informar o número de imagens que deseja buscar")
     }
@@ -16,4 +32,19 @@ botaoPesquisar.addEventListener('click', function (evento) {
 
 function atualizaPagina() {
     window.location.reload();
+}
+
+function renderizaFotosCachorrosDom(fotosCachorrosLista) {
+    //Percorrendo o Array com as fotos
+    for (let cachorroImg of fotosCachorrosLista.message) {
+
+        //Cria um novo elemento img no html
+        let img = document.createElement("img");
+
+        //Adiciona a URL da imagem do cachorro ao elemento img
+        img.src = cachorroImg;
+
+        //Adiciona a img a lista (elemento pai)
+        exibeResultado.appendChild(img);
+    }
 }
